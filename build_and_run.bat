a931bbf010fbb8ee38141d8520f82a9af0bde652
Carbone13 <lucas.michaudel@gmail.com>
2023-04-15 23:45:09 +0200

    initial commit

---
+++ build_and_run.bat
@@ -0,0 +1,5 @@
+@echo OFF
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+cmake -G Ninja -S . -B .build -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake --build .build -j12
+.build\pesto.exe
---
+++ src/pesto.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Bonjour, Monde !" << std::endl;
+
+    return 0;
+}
